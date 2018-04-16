32ab1bb34aad5a5c0304d1861baddba21db05d19
David Häfele <david.haefele98@gmail.com>
2018-04-16 18:31:10 +0200

    Getting rid of PHP

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 @Component({
   selector: 'page-home',
@@ -10,23 +9,10 @@ import 'rxjs/add/operator/map';
 export class HomePage {
   user: any = this.user;
   pass: any = this.pass;
-  dataset: any = this.dataset;
-  testdata: string = "";
-  querystring: any = "Changeme";
-  stringtest: any = "Moje";
 
-  constructor(public navCtrl: NavController, public http: Http) {
+  constructor(public navCtrl: NavController) {
   }
 
   getFunc() {
-    this.testdata = "Function executed";
-
-    this.querystring = {
-      phpdata: (): Promise<any> => {
-        return this.http.get("./home.php").map(stringtest => {
-          return stringtest.json();
-        }).toPromise();
-      }
-    }
   }
 }
