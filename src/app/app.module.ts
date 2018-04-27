40a5b72b4c3417ca3ab81d6a34b0825e08dfd730
David Häfele <david.haefele98@gmail.com>
2018-04-27 22:56:17 +0200

    Minor changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,6 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 
 @NgModule({
@@ -40,8 +39,6 @@ import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    SQLite,
-    SQLiteObject
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 @Component({
   selector: 'page-home',
@@ -10,19 +9,8 @@ export class HomePage {
   user: any = this.user;
   pass: any = this.pass;
 
-  constructor(public navCtrl: NavController, private sqlite: SQLite) {
-      this.sqlite.create({
-          name: 'data.db',
-          location: 'default'
-      })
-          .then((db: SQLiteObject) => {
+  constructor(public navCtrl: NavController) {
 
-              db.executeSql('create table users(name VARCHAR(32))', {})
-                  .then(() => console.log('Executed SQL'))
-                  .catch(e => console.log(e));
-
-          })
-          .catch(e => console.log(e));
   }
 
   getFunc() {
