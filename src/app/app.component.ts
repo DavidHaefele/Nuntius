8fcca7c6791e016bdc53239a47cbad38af884259
ThelastCookie <superkommentar@gmail.com>
2018-05-26 09:40:53 +0200

    fixed Logout not working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform, App, MenuController } from 'ionic-angular';
+import { Component, ViewChild  } from '@angular/core';
+import { Platform, App, MenuController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { Welcome } from '../pages/welcome/welcome';
@@ -9,8 +9,9 @@ import { SplitPane } from '../providers/split-pane';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = Welcome;
+  rootPage: any = Welcome;
 
+  @ViewChild(Nav) nav: Nav; 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public splitPane: SplitPane, public app: App, public menu: MenuController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -24,12 +25,12 @@ export class MyApp {
     const root = this.app.getRootNav();
     this.menu.enable(false);
     this.splitPane.splitPaneState = false;
-    root.popToRoot();
+    this.nav.setRoot(this.rootPage);
+    //root.popToRoot();
   }
 
   logout() {
     //Api Token Logout 
-
     localStorage.clear();
     this.menu.enable(false);
     setTimeout(() => this.backToWelcome(), 1000);
